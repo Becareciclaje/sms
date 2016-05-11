d86cd4c4edebaae409240b01937671b9e5f95f1c
sms <bacareciclaje@gmail.com>
2016-05-11 09:58:22 +0200

    segundo de lino

--- src/com/gestor/sms/confuguracion/ServletContext.java
+++ src/com/gestor/sms/confuguracion/ServletContext.java
@@ -1,7 +1,7 @@
 package com.gestor.sms.confuguracion;
 
 import java.util.Properties;
-//lino
+//lino1
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
