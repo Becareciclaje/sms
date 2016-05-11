bcfc780ceb952d0f06b9ba61366760320d695b3e
sms <bacareciclaje@gmail.com>
2016-05-11 10:06:03 +0200

    arreglo bea

--- src/com/gestor/sms/confuguracion/ServletContext.java
+++ src/com/gestor/sms/confuguracion/ServletContext.java
@@ -1,7 +1,10 @@
+
 package com.gestor.sms.confuguracion;
 
 import java.util.Properties;
 //prueba
+//Fer
+//esto es de lino
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
