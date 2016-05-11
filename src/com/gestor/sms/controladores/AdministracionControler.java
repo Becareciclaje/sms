dc5a8db62e32daac63c47563074a9ac20d61360e
javierdiaz <javierdiaz@192.168.120.21>
2016-05-11 13:26:16 +0200

    Controladores separados

---
+++ src/com/gestor/sms/controladores/AdministracionControler.java
@@ -0,0 +1,16 @@
+package com.gestor.sms.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdministracionControler
+{
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home";
+	}
+
+}
