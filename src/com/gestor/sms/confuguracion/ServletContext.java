244de2f33e6ab5a720cc11eac311534c71006bce
sms <bacareciclaje@gmail.com>
2016-05-11 09:57:24 +0200

    cambio lino

--- src/com/gestor/sms/confuguracion/ServletContext.java
+++ src/com/gestor/sms/confuguracion/ServletContext.java
@@ -1,7 +1,7 @@
 package com.gestor.sms.confuguracion;
 
 import java.util.Properties;
-//prueba
+//lino
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
