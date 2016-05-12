44b0f291877d0be44fa49e5d8991204e25882c6f
javierdiaz <javierdiaz@192.168.120.21>
2016-05-12 10:31:27 +0200

    creacion de aspectos, servicios y daos

--- src/com/gestor/sms/confuguracion/ServletContext.java
+++ src/com/gestor/sms/confuguracion/ServletContext.java
@@ -20,60 +20,54 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-
-
 @Configuration
 @EnableWebMvc
 @ComponentScan(
-	{ "com.gestor.sms.controladores", 
-		})
+	{ "com.gestor.sms.controladores", "com.gestor.sms.servicios", "com.gestor.sms.daos",
+			"com.gestor.sms.aspectos", })
 @EnableAspectJAutoProxy
 public class ServletContext extends WebMvcConfigurerAdapter
 {
 	@Bean
 	public DriverManagerDataSource getDriverManagerDataSource()
 	{
-		DriverManagerDataSource dataSource= new DriverManagerDataSource();
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.
-		setUrl("jdbc:mysql://192.168.120.28:3306/"
-				+ "gestorsms?useUnicode=true&useLegacyDatetime"
+		dataSource.setUrl("jdbc:mysql://192.168.120.28:3306/" + "gestorsms?useUnicode=true&useLegacyDatetime"
 				+ "Code=false&serverTimezone=UTC");
 		dataSource.setUsername("root");
-		dataSource.setPassword("tecnocom");	
+		dataSource.setPassword("tecnocom");
 		return dataSource;
 	}
-	
+
 	@Bean
 	public LocalSessionFactoryBean getLocalSessionFactoryBean()
 	{
 		LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
 		factoryBean.setDataSource(getDriverManagerDataSource());
 		factoryBean.setPackagesToScan("curso.tecnocom.spring.datos");
-		Properties properties= new Properties();
+		Properties properties = new Properties();
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.format_sql", "true");
 		properties.setProperty("hibernate.id.new_generator_mappings", "false");
 		factoryBean.setHibernateProperties(properties);
-		return factoryBean;		
+		return factoryBean;
 	}
-	
+
 	@Bean
 	public InternalResourceViewResolver getInternalResourceViewResolver()
 	{
-		InternalResourceViewResolver internalResourceViewResolver= 
-				new InternalResourceViewResolver();
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
 		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
-		internalResourceViewResolver.setSuffix(".jsp");		
+		internalResourceViewResolver.setSuffix(".jsp");
 		return internalResourceViewResolver;
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry)
 	{
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
-	
+
 }
