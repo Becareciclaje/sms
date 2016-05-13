fb9ac6d7bef72ad7eae8cc1ed6aa15f1f485b8c3
sms <bacareciclaje@gmail.com>
2016-05-13 10:53:28 +0200

    Subir SmsControler

--- src/com/gestor/sms/controladores/SMSControler.java
+++ src/com/gestor/sms/controladores/SMSControler.java
@@ -2,15 +2,20 @@ package com.gestor.sms.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gestor.sms.datos.Usuario;
 
 @Controller
 public class SMSControler
 {
 	
 	@RequestMapping("/")
-	public String home()
+	public ModelAndView home()
 	{
-		return "home";
+		ModelAndView modelAndView= new ModelAndView("login");
+		modelAndView.addObject("usuario", new Usuario());
+		return modelAndView;
 	}
 
 }
