f48e48b586683cdc0c183f7b4e22f352a5d7a0b9
sms <bacareciclaje@gmail.com>
2016-05-13 13:58:24 +0200

    modificaciones login

--- src/com/gestor/sms/controladores/SMSControler.java
+++ src/com/gestor/sms/controladores/SMSControler.java
@@ -2,9 +2,12 @@ package com.gestor.sms.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gestor.sms.datos.Usuario;
+import com.gestor.sms.servicios.SmsService;
+
 
 @Controller
 public class SMSControler
@@ -16,6 +19,29 @@ public class SMSControler
 		ModelAndView modelAndView= new ModelAndView("login");
 		modelAndView.addObject("usuario", new Usuario());
 		return modelAndView;
+	}
+	private SmsService smsService;
+	@RequestMapping(value = "/verLogin", method = RequestMethod.POST)
+	public String verLogin(Usuario login)
+		{
+		try
+		{
+			getSmsService().verificaLogin(login);
+			return "menu";
+		} catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "errorLogin";
+		}
+	}
+	public SmsService getSmsService()
+	{
+		return smsService;
+	}
+	public void setSmsService(SmsService smsService)
+	{
+		this.smsService = smsService;
 	}
 
 }
