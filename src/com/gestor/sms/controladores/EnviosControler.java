27e386dae5699a52abfe6d967b29460a42438662
sms <bacatecnocom@gmail.com>
2016-05-13 10:02:04 +0200

    agrupamiento

--- src/com/gestor/sms/controladores/EnviosControler.java
+++ src/com/gestor/sms/controladores/EnviosControler.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EnviosControler
 {
-	
-	@RequestMapping("/")
-	public String home()
+	@RequestMapping("/envios")
+	public String envios()
 	{
-		return "home";
+		return "envios";
 	}
+	
 
 }
