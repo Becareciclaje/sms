7d675393081c7357bc7e6962807dd8ee19abf0be
sms <bacareciclaje@gmail.com>
2016-05-13 14:25:09 +0200

    jorge

--- src/com/gestor/sms/daos/ComprasDao.java
+++ src/com/gestor/sms/daos/ComprasDao.java
@@ -2,23 +2,28 @@ package com.gestor.sms.daos;
 
 import java.util.List;
 
+
+
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import com.gestor.sms.datos.Cuenta;
 
-@Repository
+
 public class ComprasDao extends GestorDao
 {
 
+	@SuppressWarnings("unchecked")
 	public void dameCuentas(List<Cuenta> cuentas, int id)
 	{
-		Criteria criteria=getSession().createCriteria(Cuenta.class);
+		//Criteria criteria=getSession().createCriteria(Cuenta.class);
+		Query criteria=getSession().createQuery("from Cuenta c");
+		//criteria.add(Restrictions.eq("id", id));
+		List<Cuenta> c=criteria.list();
 		
-		//criteria.add(Restrictions.eqOrIsNull("id", id));
-		
-		cuentas=criteria.list();
+		cuentas=c;
 		
 	}
 	
