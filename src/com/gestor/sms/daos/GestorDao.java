66cd57cb24575c35beeab9de4dacbdaf154bba61
sms <bacatecnocom@gmail.com>
2016-05-13 14:22:44 +0200

    gestor completo para compras con servicios y daos y arreglo de la confuguracion de conexion

--- src/com/gestor/sms/daos/GestorDao.java
+++ src/com/gestor/sms/daos/GestorDao.java
@@ -6,7 +6,7 @@ import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public class GestorDao
 {
 	@Autowired
